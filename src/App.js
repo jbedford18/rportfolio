9cacfe410d76bc01ddb579212b056382185ec224
Justin Bedford <jbedford94@gmail.com>
2021-11-12 22:27:34 -0500

    home pg

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navi from './components/Navi';
+import Home from './components/Home';
 import About from './components/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -11,6 +12,7 @@ function App() {
     <div>
       <Navi></Navi>
       <main>
+        <Home></Home>
         <About></About>
       </main>
     </div>
