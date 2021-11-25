cff5fdc1ff6c1d7ce80071c0aa32e1c127d3dfcb
Justin Bedford <jbedford94@gmail.com>
2021-11-24 21:29:13 -0500

    deplio

--- src/components/Navi.js
+++ src/components/Navi.js
@@ -8,7 +8,7 @@ const Navi = () => {
   <>
   <Navbar collapseOnSelect fixed='top' expand="lg" bg="dark" variant="dark">
   <Container>
-  <Navbar.Brand as={Link} to ='/'>Justin Bedford</Navbar.Brand>
+  <Navbar.Brand as={Link} to ='/rportfolio'>Justin Bedford</Navbar.Brand>
   <Navbar.Toggle aria-controls="responsive-navbar-nav" />
   <Navbar.Collapse id="responsive-navbar-nav">
     <Nav>
